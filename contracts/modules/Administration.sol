37a79637e21ad8235b821dec16340269a133d815
Postables <alextrottier2014@gmail.com>
2017-10-21 22:32:19 -0700

    administration library update

--- contracts/modules/Administration.sol
+++ contracts/modules/Administration.sol
@@ -1,11 +1,18 @@
 pragma solidity 0.4.18;
 
-// Used for function invoke restriction
+/** Built-in README
+    
+    * Developers    : Postables
+    * Version       : 0.0.2alpha
+    * Notes: Used for contract adminstration, and delegation of administrative duties 
+**/
+
 contract Administration {
 
-    address     public owner; // temporary address
+    address     public owner;
     
     mapping (address => bool) public moderators;
+    mapping (address => string) privilegeStatus;
 
     event AddMod(address indexed _invoker, address indexed _newMod, bool indexed _modAdded);
     event RemoveMod(address indexed _invoker, address indexed _removeMod, bool indexed _modRemoved);
@@ -21,27 +28,46 @@ contract Administration {
 
     modifier onlyOwner() {
         require(msg.sender == owner);
-        _; // function code inserted here
+        _;
     }
 
-    function transferOwnership(address _newOwner) public onlyOwner returns (bool success) {
+    function transferOwnership(address _newOwner)
+        public
+        onlyOwner
+        returns (bool success)
+    {
         owner = _newOwner;
         return true;
         
     }
 
-    function addModerator(address _newMod) public onlyOwner returns (bool added) {
+    function addModerator(address _newMod)
+        public
+        onlyOwner
+        returns (bool added)
+     {
         require(_newMod != address(0x0));
         moderators[_newMod] = true;
         AddMod(msg.sender, _newMod, true);
         return true;
     }
     
-    function removeModerator(address _removeMod) public onlyOwner returns (bool removed) {
+    function removeModerator(address _removeMod)
+        public
+        onlyOwner
+        returns (bool removed)
+    {
         require(_removeMod != address(0x0));
         moderators[_removeMod] = false;
         RemoveMod(msg.sender, _removeMod, true);
         return true;
     }
 
+    function getRoleStatus(address _addr)
+        public
+        view  // We use view as we promise to not change state, but are reading from a state variable
+        returns (string _role)
+    {
+        return privilegeStatus[_addr];
+    }
 }
